1f8a9efcdb21d218b93304c586e220254890d698
Ariel Serruya <aserruya@multisensorscientific.com>
2022-02-24 18:27:47 -0500

    Basic SearchBar and ResultCard components. Basic file structure changes and test placeholders. Material UI imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import SearchBar from "./common/components/SearchBar/SearchBar";
+import ResultCard from "./common/components/ResultCard/ResultCard";
+import Pagination from "@mui/material/Pagination";
+import { search } from "./common/apis/githubAPI";
 
 function App() {
+  const pageCount = 30;
+  const [results, setResults] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handlePage = (event, value) => {
+    setPage(value);
+  };
+
+  const handleSearch = () => {
+    search(searchTerm, pageCount, page)
+      .then((response) => {
+        setResults(response.data.items);
+        setTotalResults(response.data.total_count);
+        console.log("Github Search API Success:", response);
+      })
+      .catch((error) => {
+        console.log("Github Search API Failed:", error);
+      })
+      .finally(() => {
+        console.log("Github Search API Finished");
+      });
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [page]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        width: "100%",
+        overflow: "auto",
+      }}
+    >
+      <SearchBar
+        handleSearch={handleSearch}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+      />
+
+      {results.map((result) => {
+        return <ResultCard key={result.id} result={result} />;
+      })}
+
+      {results.length > 0 && (
+        <Pagination
+          count={Math.ceil(totalResults / pageCount)}
+          page={page}
+          onChange={handlePage}
+        />
+      )}
     </div>
   );
 }
